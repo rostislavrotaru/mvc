7d9d3575b958b5ead2d403c2adf05c7289645ec3
rostislavrotaru <rostislav.rotaru@rossoft.md>
2012-12-04 17:16:45 +0200

    Removed unnecessary test modules
    Change-Id: I72961cdb0eddc1b0d3fd91f58e6d9ee2a5a32b42

--- app/modules/security/themes/default/layouts/login.php
+++
@@ -1,5 +0,0 @@
-this is login layout from security module theme <br />
-<?php
-	use Spherus\HttpContext\HttpContext;
-	echo HttpContext::getPageContent(); 
-?>
--- app/modules/test/views/home/index.php
+++
@@ -1,5 +0,0 @@
-<?php
-	echo('index view from test module<br />');
-?>
-<a href="/main/home/redirect">redirect</a><br />
-<a href="/main/home/about">about</a>
