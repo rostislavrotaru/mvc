084297d42f644df85c4315063838038ba325c3ac
rostislavrotaru <rostislav.rotaru@rossoft.md>
2012-12-05 17:58:54 +0200

    Change-Id: Ic88eece62e1d7b99702ce76b7cd38d8c2c905478

--- index.php
+++ index.php
@@ -7,7 +7,7 @@
 	 * Redistributions of files must retain the above copyright notice.
 	 *
 	 * @author 			Rostislav Rotaru <rostislav.rotaru@spherus.net> 
-	 * @copyright		Copyright, SPHERUS (http://spherus.net)
+	 * @copyright		SPHERUS (http://spherus.net)
 	 * @link			http://spherus.net
 	 * @since			SPHERUS v 3.0.0
 	 * @license			OSD License (http://www.spherus.net/license)
