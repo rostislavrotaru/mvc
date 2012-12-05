4e034e664d6130d1f8ff12c94036c2062afc90e3
rostislavrotaru <rostislav.rotaru@rossoft.md>
2012-12-05 17:55:59 +0200

    Refactored comments
    Change-Id: I06b236c4037498627c09c643be06c10fc63e4219

--- index.php
+++ index.php
@@ -2,13 +2,15 @@
 
 	/**
 	 * Main entry point 
-	 * 
-	 * @version 3.0
-	 * @package Public
-	 * @author Rostislav Rotaru 
-	 * @link http://spherus.net
-	 * @license See license file
-	 * 
+	 *
+	 * Licensed under the OSD license
+	 * Redistributions of files must retain the above copyright notice.
+	 *
+	 * @author 			Rostislav Rotaru <rostislav.rotaru@spherus.net> 
+	 * @copyright		Copyright, SPHERUS (http://spherus.net)
+	 * @link			http://spherus.net
+	 * @since			SPHERUS v 3.0.0
+	 * @license			OSD License (http://www.spherus.net/license)
 	 */
 	//Require the main file
 	require_once('spherus'.DIRECTORY_SEPARATOR.'main.php');
