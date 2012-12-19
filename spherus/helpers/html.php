0c390ea8679d4cba69ad1f61387dfe3821e66af3
rostislavrotaru <rostislav.rotaru@rossoft.md>
2012-12-19 17:03:28 +0200

    Changed for PageProcessor functionality

--- spherus/helpers/html.php
+++ spherus/helpers/html.php
@@ -8,14 +8,14 @@
 	* @link http://spherus.net
 	* @since 3.0
 	* 
-	* test comment
 	*/
 
 	namespace Spherus\Helpers
 	{
 	
+		use Spherus\Core\PageProcessor;
 		use Spherus\Core\SpherusException;
-
+		use Spherus\Core\Context;
 		use Spherus\Core\Check;
 
 		/**
@@ -29,12 +29,6 @@
 		{
 		    
 		    /* VARIABLES */
-		    
-			/**
-			 * CSS tags list. Stores all css files links till the moment that will be used.
-			 * @var array
-			 */
-			private static $cssTagList = array();
 			
 			/**
 			 * Javascript tags list. Stores all javascript files links till the moment that will be used.
@@ -56,7 +50,7 @@
 			 * 
 			 * @return string
 			 */
-			protected static function HtmlTagAttributeToString($name, $value)
+			private static function HtmlTagAttributeToString($name, $value)
 			{
 				Check::IsNullOrEmpty($name);
 			    Check::IsNullOrEmpty($value);
@@ -70,7 +64,7 @@
 			 * @param string|array $attributes tag attributes in string or $name=>$value format.
 			 * @return string
 			 */
-			protected static function HtmlTagAttributesToString($attributes)
+			private static function HtmlTagAttributesToString($attributes)
 			{
 				if (is_array($attributes)) 
 				{
@@ -86,21 +80,6 @@
 				{
 					return $attributes;
 				}
-			}
-			
-			/**
-			 * Gets CSS include string if it was not included already
-			 * 
-			 * @param string $filePath path to file
-			 */
-			private static function CssProcess($filePath)
-			{
-			    $cssString = self::GetLink(array('type' => 'text/css', 'rel' => 'stylesheet', 'href' => $filePath.'.css'));
-	        	if (!in_array($cssString, self::$cssTagList))
-				{
-					self::$cssTagBuffer[] = $cssString;
-					return $cssString;
-				}		    
 			}
 			
 			/**
@@ -137,106 +116,25 @@
 			    return '<'.$tag.self::HtmlTagAttributesToString($attributes).'>'.$value.'</'.$tag.'>';
 			}
 			
-			
-			/* META TAGS */
-			
-			/**
-			 * Generates 'meta' tag
-			 * 
-			 * @param string $name parameter name ('name' or 'http-equiv')
-			 * @param string $value parameter value
-			 * @param string $content the content of the meta information
-			 * @param string|array $attributes tag attributes
-			 */
-			public static function MetaTag($name, $value, $content, $attributes = null)
-			{
-				$attributes = self::GetAttributeString('content', $content).self::HtmlTagAttributeToString($name, $value).self::HtmlTagAttributesToString($attributes);
-				return self::HtmlTag('meta', $attributes);
-			}
-			
-			/**
-			 * Generates 'title' tag
-			 * 
-			 * @param string $value text to be displayed
-			 * @param string|array $attributes tag attributes
-			 */
-			public static function Title($value, $attributes = null)
-			{
-				return self::HtmlTag('title', $attributes, $value);	
-			}
-			
 			/**
 			 * Generates 'link' tag
-			 * 
+			 *
 			 * @param string|array $attributes tag attributes
 			 */
 			public static function Link($attributes)
 			{
 			    return self::HtmlTag('link', $attributes);
 			}
-	
-			/**
-			 * Generates png favicon
-			 * 
-			 * @param string $fileName path to the favicon png image
-			 */
-			public static function FaviconPng($fileName)
-			{
-			    return self::Favicon($fileName, 'image/png');
-			}
-			
-			/**
-			 * Generates ico favicon
-			 *
-			 * @param string $fileName path to the favicon ico image
-			 */
-			public static function FaviconIco($fileName)
-			{
-			    return self::Favicon($fileName, 'image/x-icon');
-			}
-			
-			/**
-			 * Generates and returns favicon tag
-			 *
-			 * @param string $fileName path to the favicon image
-			 * @param string $iconType The type of icon
-			 */
-			public static function Favicon($fileName, $iconType)
-			{
-			    Check::FileIsReadable($fileName);
-			    Check::IsNullOrEmpty($iconType);
-			    
-			    return self::Link(array('rel' => 'shortcut icon', 'href' => $fileName, 'type' => $iconType));
-			}
-			
-			/**
-			 * Generates 'style' tag
-			 * 
-			 * @param string|array $attributes tag attributes
-			 */
-			public static function Style($value, $attributes = '')
-			{
-				self::HtmlTag('style', $attributes, $value);
-			}
 			
 			/**
 			 * Includes CSS file by generating appropriate link tag
 			 * 
-			 * @param string|array $pathToFile path to the CSS file
+			 * @param string $fileName path to the CSS file
 			 */
-			public static function Css($pathToFile)
-			{
-			    if (is_array($pathToFile))
-			    {
-			        foreach ($pathToFile as $filePath)
-			        {
-			            echo self::CssProcess($filePath);
-			        }
-			    }
-			    else
-			    {
-	    			echo self::CssProcess($pathToFile);
-			    }
+			public static function Css($fileName)
+			{			    
+			    require_once(CORE.'pageprocessor.php');
+			    PageProcessor::AddCssLink($fileName);
 			}
 			
 			/**
@@ -258,387 +156,7 @@
 	    			self::Script('text/javascript', '', array('src' => $pathToFile.'.js'));
 			    }		    
 			}
-	
 			
-			/* EXTERNAL FILES (BUFFER) */
-				
-			/**
-			 * Adds CSS file(-s) into buffer
-			 * 
-			 * @param string|array $pathToFile path to the CSS file
-			*/
-			public static function CssToBuffer($pathToFile)
-			{
-			    if (is_array($pathToFile))
-			    {
-			        foreach ($pathToFile as $filePath)
-			        {
-	                    self::CssProcess($filePath);		        
-			        }
-			    }
-			    else
-			    {
-	                self::CssProcess($pathToFile);			        
-			    }
-			}
-			
-			/**
-			 * Includes CSS files from $cssTagBuffer array
-			 */
-			public static function IncludeCssBufferFiles()
-			{
-				foreach (self::$cssTagBuffer as $value)
-				{
-					echo $value;
-				}
-			}
-			
-			/**
-			 * Adds JS file(-s) into buffer
-			 * 
-			 * @param string|array $pathToFile path to the js file
-			*/
-			public static function JavaScriptToBuffer($pathToFile)
-			{
-			    if (is_array($pathToFile))
-			    {
-			        foreach ($pathToFile as $filePath)
-			        {
-	                    self::JavaScriptProcess($filePath);			        
-			        }
-			    }
-			    else
-			    {
-	                self::JavaScriptProcess($pathToFile);
-			    }
-			}
-			
-			/**
-			 * Includes JS files from $jsTagBuffer array
-			 */
-			public static function IncludeJavaScriptBufferFiles()
-			{
-				foreach (self::$jsTagBuffer as $value)
-				{
-					echo $value;
-				}
-			}
-			
-			
-			/* GENERAL TAGS */
-			
-			/**
-			 * Generates 'img' tag
-			 * 
-			 * @param string $src path to image (src HTML arrtibute value)
-			 * @param string $alt alternative text
-			 * @param string|array $attributes tag attributes
-			 */
-			public static function Img($src, $alt = '', $attributes = '')
-			{
-				$attributes = self::GetAttributeString('src', $src).self::GetAttributeString('alt', $alt).self::GetAttributesString($attributes);
-				self::TagRaw('img', $attributes);
-			}
-			
-			/**
-			 * Generates 'a' tag
-			 * 
-			 * @param string|boolean $url url (href attribute) or false in no href tag needed
-			 * @param string $content text or other content to be displayed
-			 * @param string|array $attributes tag attributes
-			 */
-			public static function Anchor($url = '', $content = '', $attributes = '')
-			{
-				$attributes = ($url === false) ? self::GetAttributesString($attributes) : self::GetAttributeString('href', $url).self::GetAttributesString($attributes);
-				self::TagRaw('a', $attributes, $content);
-			}
-			
-			/**
-			 * Generates 'div' tag 
-			 * 
-			 * @param string $name input field name
-			 * @param mixed $value input field value
-			 * @param string|array $attributes tag attributes
-			 */
-			public static function Div($content = '', $class = '', $attributes = '') 
-			{
-				self::Tag('div', self::GetAttributeString('class', $class).self::GetAttributesString($attributes), $content);
-			}
-			
-			
-			/* FORM TAGS */
-			
-		    /**
-			 * Generates a open form tag
-			 *
-			 * @param string $action form action 
-			 * @param string $method data transmission method (GET/POST)
-			 * @param string $attributes tag attributes
-			 * @param boolean $isStartTag defines whether to generate only the start form tag
-			 */		
-			public static function Form($action, $method = 'POST', $attributes = '', $isStartTag = true)
-			{
-				$attributes = self::GetAttributeString('action', $action).self::GetAttributeString('method', $method).self::GetAttributesString($attributes);
-				
-				//defines whether to generate only start form tag
-				if ($isStartTag)
-				{
-					echo "<form $attributes>";
-				}
-				else
-				{
-					self::Tag('form', $attributes);
-				}
-			}
-			
-			/**
-			 * Generates 'input' tag
-			 * 
-			 * @param string $name input field name
-			 * @param mixed $value input field value
-			 * @param string $type input field type
-			 * @param string|array $attributes tag attributes
-			 */
-			public static function Input($name, $value = '', $type = 'text', $attributes = '')
-			{
-				$attributes = self::GetAttributeString('name', $name).self::GetAttributeString('value', $value).self::GetAttributeString('type', $type).self::GetAttributesString($attributes);
-	
-				self::TagRaw('input', $attributes);
-			}
-			
-			/**
-			 * Generates 'input' tag with the 'text' type
-			 * 
-			 * @param string $name input field name
-			 * @param mixed $value input field value
-			 * @param string|array $attributes tag attributes
-			 */
-			public static function InputText($name = '', $value = '', $attributes = '') 
-			{
-				self::Input($name, $value, 'text', $attributes);
-			}
-			
-			/**
-			 * Generates 'input' tag with the 'hidden' type
-			 * 
-			 * @param string $name input field name
-			 * @param mixed $value input field value
-			 * @param string|array $attributes tag attributes
-			 */
-			public static function InputHidden($name = '', $value = '', $attributes = '') 
-			{
-				self::Input($name, $value, 'hidden', $attributes);
-			}
-			
-			/**
-			 * Generates 'input' tag with the 'password' type
-			 * 
-			 * @param string $name input field name
-			 * @param mixed $value input field value
-			 * @param string|array $attributes tag attributes
-			 */
-			public static function InputPassword($name = '', $value = '', $attributes = '') 
-			{
-				self::Input($name, $value, 'password', $attributes);
-			}
-			
-			/**
-			 * Generates 'input' tag with the 'radio' type
-			 * 
-			 * @param string $name input field name
-			 * @param mixed $value input field value
-			 * @param boolean|integer $isChecked defines whether the radio button is checked
-			 * @param string|array $attributes tag attributes
-			 */
-			public static function InputRadio($name = '', $value = '', $isChecked = false, $attributes = '') 
-			{
-				$attributes = ($isChecked) ? self::GetAttributeString('checked', 'checked').self::GetAttributesString($attributes) : $attributes;
-	
-				self::Input($name, $value, 'radio', $attributes);
-			}
-					
-			/**
-			 * Generates 'input' tag with the 'checkbox' type
-			 * 
-			 * @param string $name input field name
-			 * @param mixed $value input field value
-			 * @param boolean|integer $isChecked defines whether the checkbox is checked
-			 * @param string|array $attributes tag attributes
-			 */
-			public static function InputCheckbox($name = '', $value = '', $isChecked = false, $attributes = '') 
-			{
-				$attributes = ($isChecked) ? self::GetAttributeString('checked', 'checked').self::GetAttributesString($attributes) : $attributes;
-	
-				self::InputHidden($name, '0');
-				self::Input($name, $value, 'checkbox', $attributes);
-			}
-					
-			/**
-			 * Generates 'input' tag with the 'button' type
-			 * 
-			 * @param string $name input field name
-			 * @param mixed $value input field value
-			 * @param string|array $attributes tag attributes
-			 */
-			public static function InputButton($name = '', $value = '', $attributes = '') 
-			{
-				self::Input($name, $value, 'button', $attributes);
-			}
-	
-			/**
-			 * Generates 'input' tag with the 'submit' type
-			 * 
-			 * @param string $name input field name
-			 * @param mixed $value input field value
-			 * @param string|array $attributes tag attributes
-			 */
-			public static function InputSubmit($name = '', $value = '', $attributes = '') 
-			{
-				self::Input($name, $value, 'submit', $attributes);
-			}
-			
-			/**
-			 * Generates 'input' tag with the 'reset' type
-			 * 
-			 * @param string $name input field name
-			 * @param mixed $value input field value
-			 * @param string|array $attributes tag attributes
-			 */
-			public static function InputReset($name = '', $value = '', $attributes = '') 
-			{
-				self::Input($name, $value, 'reset', $attributes);
-			}
-			
-			/**
-			 * TODO
-			 */
-			public static function InputFile()
-			{
-			    
-			}
-			
-			/**
-			 * Generates 'input' tag with the 'image' type
-			 *
-			 * @param string $src image source
-			 * @param string $alt alternative text
-			 * @param string|array $attributes HTML attributes
-			 */
-			public static function InputImage($src, $alt = '', $attributes = '')
-			{
-			    $attributes = self::GetAttributesString(array('src' => $src, 'alt' => $alt)).self::GetAttributesString($attributes);
-				self::Input(null, null, 'image', $attributes);
-			}
-			
-			/**
-			 * Generates 'select' tag with options
-			 * 
-			 * @param string $name select name
-			 * @param array $options options array in the following format: array('0' => array('some_id' => 'value1', 'name'=>'text1', 'some_attributes'=>"disabled = 'disabled'"), '1' => array('some_id' => 'value2', 'some_name'=>'text2'))
-			 * @param string $selectedValue selected value
-			 * @param string|boolean $emptyOptionValue empty option value or false if not needed
-			 * @param string $emptyOptionText empty option text
-			 * @param string|array $attributes tag attributes
-			 */
-			public static function Select($name, array $options = array(), $selectedValue = '', $emptyOptionValue = '', $emptyOptionText = '-', $attributes = '') 
-			{
-				$attributes = self::GetAttributeString('name', $name).self::GetAttributesString($attributes);			
-				
-				//empty option
-				$optionsString = ($emptyOptionValue === false) ? '' : '<option value="'.$emptyOptionValue.'">'.$emptyOptionText.'</option>';
-				
-				//loops through options
-				foreach ($options as $option)
-				{
-				    //gets option value, text, html attributes
-					$value = array_shift($option);
-					$text = array_shift($option);
-					$optionAttributes = array_shift($option);
-					
-					//defines if the option is selected
-					$selected = ($value == $selectedValue) ? self::GetAttributeString('selected', 'selected') : "";
-					
-					//all options string
-					$optionsString .= '<option value="'.$value.'"'.$selected.' '.$optionAttributes.'>'.$text.'</option>';
-				}
-				
-				self::TagRaw('select', $attributes, $optionsString);
-				
-				echo '</select>';
-			}
-					
-			/**
-			 * Generates 'textarea' tag
-			 * 
-			 * @param string $name textarea name
-			 * @param mixed $value textarea value
-			 * @param string|array $attributes tag attributes
-			 */
-			public static function Textarea($name, $value = '', $attributes = '')
-			{
-				$attributes = self::GetAttributeString('name', $name).self::GetAttributesString($attributes);
-	
-				self::TagRaw('textarea', $attributes, $value);
-			}
-	
-			/**
-			 * Generates 'label' tag
-			 * 
-			 * @param mixed $elementId value of the 'id' attribute
-			 * @param string $content label
-			 * @param string|array $attributes tag attributes
-			 * 
-			 * @return string
-			 */
-			public static function Label($elementId, $content, $attributes = '')
-			{
-				echo self::GetLabel($elementId, $content, $attributes);
-			}
-	
-			
-			/* JAVASCRIPT */
-			
-			/**
-			 * Generates 'script' tag
-			 * 
-			 * @param string $type script type
-			 * @param string $value script to be executed
-			 * @param string|array $attributes tag attributes
-			 */
-			public static function Script($type = 'text/javascript', $value, $attributes = '')
-			{
-				$attributes = "type = '$type' ".self::GetAttributesString($attributes);
-				
-				self::TagRaw('script', $attributes, $value);
-			}
-	
-			
-			/* SPHERUS DEPENDANT TAGS */
-			
-			/**
-			 * Generates 'img' tag with predefined path base (see $imageBasePath)
-			 * 
-			 * @param string $src path to image (src HTML arrtibute value)
-			 * @param string $alt alternative text
-			 * @param string|array $attributes tag attributes
-			 */
-			public static function Image($src, $alt = '', $attributes = '')
-			{
-				self::Img(self::$imageBasePath.$src, $alt, $attributes);
-			}	
-	
-			/**
-			 * Generates 'img' tag with language dependent predefined path base (see $imageLocalizedBasePath)
-			 * 
-			 * @param string $src path to image (src HTML arrtibute value)
-			 * @param string $alt alternative text
-			 * @param string|array $attributes tag attributes
-			 */
-			public static function ImageLocalized($src, $alt = '', $attributes = '')
-			{
-				self::Img(self::$imageLocalizedBasePath.$src, $alt, $attributes);
-			}		
-	
 		}
 	
 	}
