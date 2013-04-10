1b32bfad287106c337654c4d3df2356ce86fd9a1
rostislavrotaru <rostislav.rotaru@rossoft.md>
2013-04-10 14:16:18 +0300

    Added specific sql and no-sql engines

---
+++ Spherus/Data/Engine/SqlEngine/SqlDatabaseEngine.php
@@ -0,0 +1,22 @@
+<?php
+
+	/**
+	 * Redistributions of files must retain the above copyright notice.
+	 *
+	 * @copyright SPHERUS (http://spherus.net)
+	 * @license http://license.spherus.net
+	 * @link http://spherus.net
+	 * @since 3.0
+	 */
+	namespace Spherus\Data\Engine\SqlEngine;
+
+    /**
+     * Class that represents the sql database engine
+     *
+     * @author Rostislav Rotaru (rostislav.rotaru@spherus.net)
+     * @package spherus.data
+     */
+    class SqlDatabaseEngine
+    {
+
+    }
