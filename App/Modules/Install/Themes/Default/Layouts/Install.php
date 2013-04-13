f18f5bd3b272048fdab4eb96aee8c0f0ad1d33b9
rostislavrotaru <rostislav.rotaru@rossoft.md>
2013-04-13 19:40:17 +0300

    Deleted Install module

--- App/Modules/Install/Themes/Default/Layouts/Install.php
+++
@@ -1,4 +0,0 @@
-<!DOCTYPE html>
-<?php
-use Spherus\HttpContext\HttpContext;
-echo HttpContext::getPageContent();
--- App/Modules/Install/Views/Home/Index.php
+++
@@ -1,9 +0,0 @@
-<?php
-use Spherus\Core\Workbench;
-
-echo ('index view<br />');
-echo ('Current layout: ' . Workbench::getCurrentController()->layout . '<br />');
-?>
-<a href="/">index</a>
-<br />
-
