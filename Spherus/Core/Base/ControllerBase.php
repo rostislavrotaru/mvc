b78a43fcf041a74a42f2a8c79d29113e2645648a
rostislavrotaru <rostislav.rotaru@rossoft.md>
2013-04-26 12:36:11 +0300

    Refactored according OOP

--- Spherus/Core/Base/ControllerBase.php
+++ Spherus/Core/Base/ControllerBase.php
@@ -10,7 +10,10 @@
 	 */
 	namespace Spherus\Core\Base;
 
-	/**
+	use Spherus\Core\Check;
+	use Spherus\Core\SpherusException;
+	
+		/**
 	 * Class that represents the base for all application controllers
 	 *
 	 * @author Rostislav Rotaru (rostislav.rotaru@spherus.net)
@@ -22,25 +25,90 @@
 		/* FIELDS */
 
 		/**
-		 * Determine the list of controller functions that has no views
+		 * Defines whether the controller has no view. Default is False.
 		 *
-		 * @var array
+		 * @var boolean
 		 */
-		public $noViewControllers = array();
+		private $noView = false;
 
 		/**
 		 * Defines the controller layout
 		 *
 		 * @var string
 		 */
-		public $layout = null;
+		private $layout = null;
 
 		/**
-		 * Defines whether the Workbench should use IoC for loading views. 
+		 * Defines whether the Workbench should use IoC for loading controller view. 
 		 *
 		 * @var boolean
 		 */
-		public $useIocForViews = false;
+		private $useIoCForView = false;
+		
+		
+		/* PROPERTIES */
+		
+		/**
+		 * Gets whether the controller has no view.
+		 *
+		 * @var boolean
+		 */
+		public function getNoView()
+		{
+			return $this->noView;
+		}
+		
+		/**
+		 * Sets whether the controller has no view.
+		 * 
+		 * @param boolean $noView The boolean value
+		 */
+		public function setNoView($noView)
+		{
+			$this->noView = (boolean)$noView;
+		}
+		
+		/**
+		 * Gets the controller layout
+		 *
+		 * @var string
+		 */
+		public function getLayout()
+		{
+			return $this->layout;
+		}
+		
+		/**
+		 * Sets the controller layout
+		 * 
+		 * @param string $layout The layout to set
+		 * @throws SpherusException When $layout parameter is null or empty
+		 */
+		public function setLayout($layout)
+		{
+			Check::IsNullOrEmpty($layout);
+			$this->layout = $layout;
+		}
+		
+		/**
+		 * Gets whether the Workbench should use IoC for loading controller view. 
+		 *
+		 * @var boolean
+		 */
+		public function getUseIoCForView()
+		{
+			return $this->useIoCForView;
+		}
+		
+		/**
+		 * Sets whether the Workbench should use IoC for loading controller view.
+		 * 
+		 * @param boolean $useIoCForView The Boolean value
+		 */
+		public function setUseIoCForView($useIoCForView)
+		{
+			$this->useIoCForView = (boolean)$useIoCForView;
+		}
 		
 		
 		/* EVENT TEMPLATES */
@@ -48,7 +116,7 @@
 		/**
 		 * Is raised before any controller and performs common for all
 		 * controllers actions,
-		 * can be replaced or completed in application controller or definite
+		 * can be replaced or completed in application common controller or specific
 		 * controller
 		 */
 		public function BeforeLoad()
@@ -81,14 +149,5 @@
 		public function AfterAction()
 		{
 		}
-
-		/* PUBLIC METHODS */
-
-		/**
-		 * Include current controller helpers
-		 */
-		public function IncludeHelpers()
-		{
-
-		}
+	
 	}
