f786eb250c10e21695974db066447e963313082e
rostislavrotaru <rostislav.rotaru@rossoft.md>
2013-04-27 14:42:28 +0300

    Added class ComponentBase

---
+++ Spherus/Core/Base/ComponentBase.php
@@ -0,0 +1,22 @@
+<?php
+
+	/**
+	 * Redistributions of files must retain the above copyright notice.
+	 *
+	 * @copyright SPHERUS (http://spherus.net)
+	 * @license http://license.spherus.net
+	 * @link http://spherus.net
+	 * @since 3.0
+	 */
+	namespace Spherus\Core\Base;
+	
+	/**
+	 * Class that represents the base for all application components
+	 *
+	 * @author Rostislav Rotaru (rostislav.rotaru@spherus.net)
+	 * @package spherus.core.base
+	 */
+	abstract class ComponentBase
+	{
+		
+	}
