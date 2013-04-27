4a3044319c19b8941f32466472bb1ac710add87d
rostislavrotaru <rostislav.rotaru@rossoft.md>
2013-04-27 21:28:26 +0300

    Added suplimentall functionality.

--- Spherus/Core/Base/ComponentBase.php
+++ Spherus/Core/Base/ComponentBase.php
@@ -10,6 +10,9 @@
 	 */
 	namespace Spherus\Core\Base;
 	
+	use Spherus\Core\Check;
+	use Spherus\Core\SpherusException;
+	
 	/**
 	 * Class that represents the base for all application components
 	 *
@@ -19,6 +22,16 @@
 	abstract class ComponentBase
 	{
 		
+		/**
+		 * Initializes a new instance of ComponentBase class
+		 * 
+		 * @param string $name The name of component
+		 */
+		public function __construct($name)
+		{
+			$this->setName($name);
+		}
+		
 		/* FIELDS */
 		
 		/**
@@ -27,6 +40,13 @@
 		 * @var string
 		 */
 		private $name = null;
+		
+		/**
+		 * Defines an array of depending ComponentBase objects.
+		 *
+		 * @var array
+		 */
+		private $dependingComponents = []; 
 		
 		
 		/* PROPERTIES */
@@ -45,10 +65,77 @@
 		 * Sets the component name.
 		 * 
 		 * @param string $name The component name
+		 * @throws SpherusException When $name parameter is null or empty.
 		 */
 		public function setName($name)
 		{
+			Check::IsNullOrEmpty($name);
 			$this->name = $name;
 		}
+	
+		/**
+		 * Gets an array of depending ComponentBase objects.
+		 * 
+		 * @return array
+		 */
+		public function getDependingComponents()
+		{
+			return $this->dependingComponents;
+		}
+	
+		
+		/**
+		 * Gets an array of depending ComponentBase objects.
+		 * 
+		 * @param $dependingComponents an array of ComponentBase objects.
+		 */
+		public function setDependingComponents($dependingComponents)
+		{
+			$this->dependingComponents = $dependingComponents;
+		}
 
+
+		/* PUBLIC FUNCTIONS */
+		
+		/**
+		 * Adds depending component
+		 * 
+		 * @param ComponentBase $dependingComponent The depending component to add.
+		 * @throws SpherusException When component with the same name already loaded.s
+		 */
+		public function AddDependingComponent(ComponentBase $dependingComponent)
+		{
+			Check::IsNullOrEmpty($dependingComponent);
+			if($this->GetDependingComponentByName($dependingComponent->getName()) == null)
+			{
+				$this->dependingComponents[] = $dependingComponent;
+			}
+			else 
+			{
+				throw new SpherusException(printf(EXCEPTION_COMPONENT_ALREADY_LOADED, $dependingComponent->getName()));
+			}
+		}
+		
+		
+		/* PRIVATE FUNCTIONS*/
+		
+		/**
+		 * Gets depending component by name.
+		 * 
+		 * @param string $name The component name to search.
+		 * @return ComponentBase|NULL Found ComponentBase object or null
+		 */
+		private function GetDependingComponentByName($name)
+		{
+			foreach($this->dependingComponents as $component)
+			{
+				if(strtolower($component->getName()) === strtolower($name))
+				{
+					return $component;
+				}
+			}
+			
+			return null;
+		}
+	
 	}
