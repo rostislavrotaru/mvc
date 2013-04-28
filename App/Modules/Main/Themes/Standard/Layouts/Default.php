b6706830b8c2525ecbe8209f423e165049f51073
rostislavrotaru <rostislav.rotaru@rossoft.md>
2013-04-28 17:44:50 +0300

    Modified code to test styles.

--- App/Modules/Main/Themes/Standard/Layouts/Default.php
+++ App/Modules/Main/Themes/Standard/Layouts/Default.php
@@ -5,6 +5,7 @@ use Spherus\Helpers\HtmlHelper;
 use Spherus\Core\Workbench;
 
 echo HtmlHelper::Css(Workbench::getCurrentTheme()->getCssPath().SEPARATOR.'cssser');
+echo HtmlHelper::Css(Workbench::getCurrentTheme()->getCssPath().SEPARATOR.'style');
 use Spherus\HttpContext\HttpContext;
 echo HttpContext::getPageContent();
 ?>
