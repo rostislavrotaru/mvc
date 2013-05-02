92c214eb971f9a2e26183fec945b9998e7d4a5cc
rostislavrotaru <rostislav.rotaru@rossoft.md>
2013-05-02 22:57:49 +0300

    Begin development of Data component.

---
+++ Spherus/Components/Data/Component.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace Spherus\Components\Data;
+
+use Spherus\Core\Base\SystemComponentBase;
+
+/**
+ * Components class
+ * 
+ * @author rostislavrotaru
+ *
+ */
+class Component extends SystemComponentBase
+{
+	
+}
--- index.php
+++ index.php
@@ -1,1 +1,14 @@
-Index file for SPHERUS Components page.
+<?php
+
+//****************************************************
+//Replace here the path that framework is located
+set_include_path('/Volumes/DATA/Programming/SPHERUS');
+define('ROOT', __DIR__.'/../');
+include 'Spherus/Autoloader.php';
+//****************************************************
+
+
+use Spherus\Components\Data\Component;
+$component = new Component();
+$component->setName('DataEngine');
+echo $component->getName();
