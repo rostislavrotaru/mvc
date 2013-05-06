92769c738106ca7de780891831b932fdc31cc3fb
rostislavrotaru <rostislav.rotaru@rossoft.md>
2013-05-06 18:30:43 +0300

    Finished alpha version of Data component.

--- Spherus/Components/Data/Component.php
+++ Spherus/Components/Data/Component.php
@@ -29,6 +29,7 @@
 		public function __construct()
 		{
 			$this->SetComponentAttributes();
+			
 		}
 		
 		
--- index.php
+++ index.php
@@ -3,12 +3,11 @@
 //****************************************************
 //Replace here the path that the framework is located
 set_include_path('/Volumes/DATA/Programming/SPHERUS');
-define('ROOT', __DIR__.'/../');
 include 'Spherus/Autoloader.php';
 //****************************************************
 
 
-
 use Spherus\Components\Data\Component;
+
 $component = new Component();
 echo $component->getDescription();
