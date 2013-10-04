c0af67e67010563c4dae3cd113e9f7432e086a6c
rostislavrotaru <rostislav.rotaru@rossoft.md>
2013-10-04 20:37:45 +0300

    Made some little tests

--- index.php
+++ index.php
@@ -2,19 +2,39 @@
 
 //****************************************************
 //Replace here the path that the framework is located
-set_include_path('D:\Programming\SPHERUS_PHP\SOURCECODE');
+set_include_path('your path here');
 include 'Spherus/Autoloader.php';
 //****************************************************
 
 
-use Spherus\Components\Query\Component;
+//use Spherus\Components\Query\Component;
 use Spherus\Components\Query\Component\SqlDatabaseQuery\SqlFactory;
+use Spherus\Components\Query\Component\SqlDatabaseQuery\Enums\SqlOrderType;
 
-$component = new Component();
-echo('<pre>');
-var_dump($component);
-echo('</pre>');
+$column1 = SqlFactory::Column('table1_id');
+$column2 = SqlFactory::Column('table1_name');
 
-$column = SqlFactory::Column('test', null, 'test_alias');
+$table1 = SqlFactory::Table('test1');
+$table1->AddColumn($column1);
+$table1->AddColumn($column2);
 
-$t =2;
+$column3 = SqlFactory::Column('table2_id');
+$column4 = SqlFactory::Column('table2_name');
+
+$table2 = SqlFactory::Table('test2');
+$table2->AddColumn($column3);
+$table2->AddColumn($column4);
+
+$tt = $table1->LeftJoin($table2, $column1, $column3);
+
+$select = SqlFactory::Select(SqlFactory::Max($column3))
+    ->Distinct()
+    ->From($table2)
+    ->Where(SqlFactory::GreatherThan($column3, 22))
+    ->GroupBy($column3, $column4)
+    ->Having(SqlFactory::Equal($column4, 'ssssss'))
+    ->OrderBy(array(
+        SqlFactory::Order($column3, SqlOrderType::Ascending),
+        SqlFactory::Order($column4, SqlOrderType::Descending)))
+    ->Take(10)
+    ->Skip(20);
