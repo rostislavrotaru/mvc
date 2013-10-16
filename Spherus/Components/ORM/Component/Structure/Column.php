21fbf66cafe1557cf8ff664fcdb77594d6fdd212
rostislavrotaru <rostislav.rotaru@rossoft.md>
2013-10-17 00:25:47 +0300

    Moved Table and Column to Structure folder.

--- Spherus/Components/ORM/Component/Structure/Column.php
+++ Spherus/Components/ORM/Component/Structure/Column.php
@@ -8,30 +8,31 @@
  * @link http://spherus.net
  * @since 3.0
  */
-namespace Spherus\Components\ORM\Base;
+namespace Spherus\Components\ORM\Structure;
 
 use Spherus\Components\ORM\Component\Enums\EntityType;
+use Spherus\Components\ORM\Base\MappedEntity;
 
 /**
- * Class that represents table entity for SPHERUS Framework
+ * Class that represents column entity for SPHERUS Framework
  * 
  * @author Rostislav Rotaru (rostislav.rotaru@spherus.net)
  * @package spherus.components.orm
  */
-class Table extends MappedEntity
+class Column extends MappedEntity
 {
     
     /* CONSTRUCTOR */
     
     /**
-    * Initializes a new instance of Table class.
+    * Initializes a new instance of Column class.
     * 
-    * @param string $name The table name
-    * @param string $storeName The store table name
+    * @param string $name The column name
+    * @param string $storeName The store column name
     */
     public function __construct($name, $storeName)
     {
         parent::__construct(EntityType::Table, $name, $storeName);
     }
-  
+    
 }
--- Spherus/Components/ORM/Component/Structure/Table.php
+++ Spherus/Components/ORM/Component/Structure/Table.php
@@ -8,30 +8,31 @@
  * @link http://spherus.net
  * @since 3.0
  */
-namespace Spherus\Components\ORM\Base;
+namespace Spherus\Components\ORM\Structure;
 
 use Spherus\Components\ORM\Component\Enums\EntityType;
+use Spherus\Components\ORM\Base\MappedEntity;
 
 /**
- * Class that represents column entity for SPHERUS Framework
+ * Class that represents table entity for SPHERUS Framework
  * 
  * @author Rostislav Rotaru (rostislav.rotaru@spherus.net)
  * @package spherus.components.orm
  */
-class Column extends MappedEntity
+class Table extends MappedEntity
 {
     
     /* CONSTRUCTOR */
     
     /**
-    * Initializes a new instance of Column class.
+    * Initializes a new instance of Table class.
     * 
-    * @param string $name The column name
-    * @param string $storeName The store column name
+    * @param string $name The table name
+    * @param string $storeName The store table name
     */
     public function __construct($name, $storeName)
     {
-        parent::__construct(EntityType::Table);
+        parent::__construct(EntityType::Table, $name, $storeName);
     }
-    
+  
 }
