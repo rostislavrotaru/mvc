47a9de090aa9c2b1d63825db399003e41bb972dc
rostislavrotaru <rostislav.rotaru@rossoft.md>
2013-10-16 23:23:36 +0300

    Added base for ORM entities and entity type enum.

---
+++ Spherus/Components/ORM/Component/Base/ORMEntity.php
@@ -0,0 +1,62 @@
+<?php
+
+/**
+ * Redistributions of files must retain the above copyright notice.
+ *
+ * @copyright SPHERUS (http://spherus.net)
+ * @license http://license.spherus.net
+ * @link http://spherus.net
+ * @since 3.0
+ */
+namespace Spherus\Components\ORM\Base;
+
+use Spherus\Core\Base\SystemComponentBase;
+use Spherus\Core\Check;
+use Spherus\Core\SpherusException;
+
+/**
+ * Class that represents base orm entity for SPHERUS Framework
+ * 
+ * @author Rostislav Rotaru (rostislav.rotaru@spherus.net)
+ * @package spherus.components.orm
+ */
+class ORMEntity extends SystemComponentBase
+{
+    
+    /* CONSTRUCTOR */
+    
+    /**
+    * Initializes a new instance of ORMEntity class.
+    * @throws SpherusException When $entity type parameter is not set.
+    */
+    public function __construct($entityType)
+    {
+        
+        Check::IsNullOrEmpty($entityType);
+        $this->entityType = $entityType;
+    }
+    
+    
+    /* FIELDS */
+		
+	/**
+	 * Defines the orm entity type.
+	 * @var string
+	 */
+	private $entityType = null;
+
+	
+	/* PROPERTIES */
+
+	/**
+	 * @return Gets the orm entity type.
+	 * 
+	 * @var string
+	 */
+	public function getEntityType() 
+	{
+		return $this->entityType;
+	}
+    
+    
+}
