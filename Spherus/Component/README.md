1ee279740b5ac6bce1f3909d13ad0883bf52b426
rostislavrotaru <rostislav.rotaru@rossoft.md>
2013-11-09 14:06:38 +0200

    deleted unused files

--- Spherus/Component/README.md
+++
@@ -1,4 +0,0 @@
-components
-==========
-
-components for spherus suite
--- Spherus/Component/index.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-//****************************************************
-//Replace here the path that the framework is located
-set_include_path('/Volumes/DATA/Programming/SPHERUS_PHP/SPHERUS_SOURCECODE');
-include 'Spherus/Autoloader.php';
-//****************************************************
-
-echo "test";
