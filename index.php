4d424a74d19af5c7144307bcd737c754a18e4453
rostislavrotaru <rostislav.rotaru@rossoft.md>
2013-11-09 13:27:23 +0200

    test

--- index.php
+++ index.php
@@ -2,6 +2,8 @@
 
 //****************************************************
 //Replace here the path that the framework is located
-set_include_path('__path_here');
+set_include_path('/Volumes/DATA/Programming/SPHERUS_PHP/SPHERUS_SOURCECODE');
 include 'Spherus/Autoloader.php';
 //****************************************************
+
+echo "test";
