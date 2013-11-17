dbf27b82e0bfadb293fb1f3bab2e21ed80cc7221
rostislavrotaru <rostislav.rotaru@rossoft.md>
2013-11-17 18:01:37 +0200

    Working on DomainModel schema

--- App/Modules/Main/Secured/Models/DomainModel.php
+++ App/Modules/Main/Secured/Models/DomainModel.php
@@ -6,6 +6,7 @@
 	use Spherus\Components\ORM\Component\SqlModel\ModelEntities;
 	use Spherus\Components\DATA\Component\Enums\DatabaseProviderType;
 	use Spherus\Components\Data\Component\DatabaseConfig;
+	use Spherus\Components\ORM\Component\Enums\PropertyType;
 								
 	class DomainModel extends ModelEntities
 	{
@@ -20,12 +21,44 @@
 							Config::$domainModelConfig['user'],
 							Config::$domainModelConfig['password']));
 
-			$this->InitializeModel();
-
 		}
 		
-		private function InitializeModel()
-		{
-			$this->AddModel(new User('User'));
-		}
+		public $schema = array
+		(
+			'Models'=>array
+			(
+				'User'=>array
+				(
+					'Name'=>'User',
+					'EntitySetName'=>'Users',
+					'TableName'=>'users',
+					'Properties'=>array
+					(
+						'Id'=>array
+						(
+							'ColumnName'=>'user_id',
+							'Type'=>PropertyType::Integer,
+							'DefaultValue'=>null,
+							'Required'=>true,
+							'Length'=>0		
+						),
+						'Name'=>array
+						(
+							'ColumnName'=>'user_name',
+							'Type'=>PropertyType::Varchar,
+							'DefaultValue'=>null,
+							'Required'=>true,
+							'Length'=>0
+						)
+					),
+					'NavigationProperties'=>array
+					(
+						'UserRoles'=>array
+						(
+							'table_name'=>'user_roles',
+						)	
+					)
+				)
+			)
+		);
 	}
--- App/Modules/Main/Secured/Models/Role.php
+++ App/Modules/Main/Secured/Models/Role.php
@@ -1,14 +1,11 @@
 <?php
 	
 	namespace App\Modules\Main\Secured\Models;
+	
+	use Spherus\Components\ORM\Component\SqlModel\Model;
 			
-	class User
+	class Role extends Model
 	{
 		public $Id = null;
 		public $Name = null;
-	}
-	
-	class UserModel
-	{
-		
 	}
