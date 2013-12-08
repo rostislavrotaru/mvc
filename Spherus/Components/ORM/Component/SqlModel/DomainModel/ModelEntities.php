30a6cc371e99a1d83d2157413ad80177d97c648d
rostislavrotaru <rostislav.rotaru@rossoft.md>
2013-12-08 16:25:54 +0200

    Corrected the ModelEntities class name.

--- Spherus/Components/ORM/Component/SqlModel/DomainModel/ModelEntities.php
+++ Spherus/Components/ORM/Component/SqlModel/DomainModel/ModelEntities.php
@@ -24,7 +24,7 @@
 	 * @author Rostislav Rotaru (rostislav.rotaru@spherus.net)
 	 * @package spherus.components.orm
 	 */
-	class ModelEntites extends Entity
+	class ModelEntities extends Entity
 	{
 		
 		/* CONSTRUCTOR */
