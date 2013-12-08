3027d2cfc57c6e1c028d68758e28f86f7ae076e2
rostislavrotaru <rostislav.rotaru@rossoft.md>
2013-12-08 16:35:39 +0200

    Added ModelEntityState enum.

---
+++ Spherus/Components/ORM/Component/SqlModel/Enums/ModelEntityState.php
@@ -0,0 +1,27 @@
+<?php
+
+	/**
+	 * Redistributions of files must retain the above copyright notice.
+	 *
+	 * @copyright SPHERUS (http://spherus.net)
+	 * @license http://license.spherus.net
+	 * @link http://spherus.net
+	 * @since 3.0
+	 */
+	namespace Spherus\Components\ORM\Component\SqlModel\Enums;
+
+	use Spherus\Core\Enums\Enum;
+					
+	/**
+     * Class that represents an ORM Model Entity state type
+     *
+     * @author Rostislav Rotaru (rostislav.rotaru@spherus.net)
+     * @package spherus.components.orm
+     */
+    class ModelEntityState extends Enum
+	{
+		const Unchanged = 'Unchanged';
+		const Unattached = 'Unattached';
+		const Changed = 'Changed';
+		const Deleted = 'Deleted';
+	}
